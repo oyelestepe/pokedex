1f7156f9d967f5998768bc9206508844a8037cec
Onurcan Yelestepe <oyelestepe1@gmail.com>
2025-07-27 12:56:11 +0300

    the fetched information was added to the application component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import PokemonList from './components/PokemonList'
 
 function App() {
 
 
   return (
     <>
-      <h1>Welcome to the Pokedex</h1>
+      <div className='flex flex-col items-center justify-center min-h-screen'>
+        <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="PokeAPI Logo" />
+        <h1 className='text-[#3761A8] font-bold'>Welcome to the Pokedex</h1>
+        <p className='text-2xl text-[#3761A8] font-bold'>Explore the world of Pokémon!</p>
+        <PokemonList />
+      </div>
     </>
   )
 }
